e3abd68cba00e892967d90064f6c8f4149246deb
azizditya@gmail.com <azizditya@gmail.com>
2022-06-23 18:01:20 +0700

    x

--- apps/src/pages/dashboard/UserInfo.js
+++ apps/src/pages/dashboard/UserInfo.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { useJwt } from "react-jwt";
 import { ContentWrapper } from ".";
-import { isLocalLogin, isLogin } from "../../helpers/util";
+import { isLogin } from "../../helpers/util";
 
 const UserInfo = () => {
   const [auth, setAuth] = useState("");
 
   const login = isLogin();
-  const localLogin = isLocalLogin();
+
+  const { decodedToken } = useJwt(login || false);
 
   const onChangeAuth = (e) => setAuth(e.target.value || "");
 
@@ -14,15 +16,23 @@ const UserInfo = () => {
     <ContentWrapper>
       <div className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl p-10 border-4 border-gray-600 text-center">
         <p className="text-3xl text-gray-700 font-bold mb-5">SUPER Dashboard</p>
-        <p className="text-xl text-gray-500 font-bold my-5">
-          {login
-            ? `Your login info: ${JSON.stringify(login)}`
-            : `You're not login`}
-        </p>
-        <p className="text-xl text-gray-500 font-bold my-5">
-          {localLogin
-            ? `Your local login: ${JSON.stringify(localLogin)}`
-            : `You're not local login`}
+        <p className="text-gray-500 text-l my-5">
+          {login ? (
+            <>
+              <p>
+                <span className="font-bold">Your login token:</span>
+                <br />
+                {JSON.stringify(login)}
+              </p>
+              <p>
+                <span className="font-bold">Your login info:</span>
+                <br />
+                {JSON.stringify(decodedToken)}{" "}
+              </p>
+            </>
+          ) : (
+            `You're not login`
+          )}
         </p>
         <form method="post" action="http://103.186.1.191">
           <input
